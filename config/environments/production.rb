c40264a1f4be853c5312fa3b7661d84c14f41d8b
Jennifer Crawshaw <jennycrawshaw@gmail.com>
2014-10-10 17:44:25 +0000

    Edit in config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,5 +81,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #http://jcrawshaw-bloccit.herokuapp.com
-  config.action_mailer.default_url_options = { host: 'jcrawshaw-bloccit.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://jcrawshaw-bloccit.herokuapp.com' }
 end
