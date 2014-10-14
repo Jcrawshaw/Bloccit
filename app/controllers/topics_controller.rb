8f16609f06f7c899c6ddcf94ebd61b05639474ec
Jennifer Crawshaw <jennycrawshaw@gmail.com>
2014-10-14 18:53:41 +0000

    Configured email in production and debugging

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -21,7 +21,7 @@ class TopicsController < ApplicationController
   end
 
  def create
-  @topic = Topic.new(params.require(:topic).permit(:name, :description, :public))
+  @topic = Topic.new(topic_params)
   authorize @topic
   if @topic.save
     redirect_to @topic, notice: "Topic was saved successfully."
@@ -30,4 +30,10 @@ class TopicsController < ApplicationController
     render :new
   end
  end
+
+ private
+
+ def topic_params
+  params.require(:topic).permit(:name, :description, :public)
+end
 end
