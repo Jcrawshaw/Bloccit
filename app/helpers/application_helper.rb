9ebab3cdaf277341ba6bbb460d9545e89ccfa64e
Jennifer Crawshaw <jennycrawshaw@gmail.com>
2014-10-19 00:27:55 +0000

    Added Redcarpet gem for markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,14 @@ module ApplicationHelper
     end
   end
 
+def markdown(text)
+  renderer = Redcarpet::Render::HTML.new
+  extensions = {fenced_code_blocks: true}
+  redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+  (redcarpet.render text).html_safe
+end
+
+
   def my_name
     name = "Jenny Crawshaw"
     name
