41b9779485509e5c8bdda7e01b6b455159e2c610
Jennifer Crawshaw <jennycrawshaw@gmail.com>
2014-10-29 00:38:26 +0000

    Debugged comments and added validation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,11 +4,18 @@ class CommentsController < ApplicationController
 def create
   @topic = Topic.find(params[:topic_id])
   @post = @topic.posts.find(params[:post_id])
-  @comments = @post.comments
-
   @comment = current_user.comments.build(comment_params)
   @comment.post = @post
-  @new_comment = Comment.new
+  if @comment.save
+    flash[:notice] = "Comment was saved."
+  else
+    flash[:error] = "There was an error saving the comment."
+  end
+  #@comments = @post.comments
+
+  # @comment = current_user.comments.build(comment_params)
+  # @comment.post = @post
+  # @new_comment = Comment.new
   redirect_to [@topic, @post]
 end
 
