42489124621a3ba0f55f3282396d53d1039be720
Jennifer Crawshaw <jennycrawshaw@gmail.com>
2014-11-05 18:18:49 +0000

    Added voting and post ranking

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,10 +17,26 @@ class Post < ActiveRecord::Base
     votes.sum(:value)
   end
   
-  default_scope { order('created_at DESC') }
+  default_scope { order('rank DESC') }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   #validates :topic, presence: true
   #validates :user, presence: true
+
+  def update_rank
+    age = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24)
+    new_rank = points + age
+
+    update_attribute(:rank, new_rank)
+  end
+
+  after_create :create_vote
+
+  private
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
+  end
+
 end
