3730df94bbdbc97669b6fc350403356e7aa1311b
Jennifer Crawshaw <jennycrawshaw@gmail.com>
2014-11-11 16:46:45 +0000

    Method for save post with intial vote

--- app/models/post.rb
+++ app/models/post.rb
@@ -38,8 +38,8 @@ class Post < ActiveRecord::Base
 
   def save_with_initial_vote
     ActiveRecord::Base.transaction do
-       post = self.post.save
-       post.create_vote
+       save
+       create_vote
     end
   end
 
